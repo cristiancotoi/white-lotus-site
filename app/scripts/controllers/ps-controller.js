87f42d6dcc6a7b258d7d705db6dfce04f91d63d2
Cristian Cotoi <cristian.cotoi@gmail.com>
2016-06-27 08:09:32 +0300

    Added challenges and opportunities

--- app/scripts/controllers/ps-controller.js
+++ app/scripts/controllers/ps-controller.js
@@ -42,93 +42,19 @@ angular
                 $scope.lc = report.lifeCycle;
                 $scope.lcd = report.lifeCycleDesc;
 
+                $scope.chalArr = report.challenges;
+                $scope.chalDesc = report.challengesDesc;
+                $scope.opportArr = report.opportunities;
+                $scope.opportDesc = report.opportunitiesDesc;
+                $scope.chAndOpIntervals = report.chAndOpIntervals;
+
                 $scope.luckChart = report.luckChartDigits.digits;
                 $scope.luckChartNumber = report.luckChartDigits.number;
+                $scope.luckChartDesc = report.luckChartDigits.descriptions;
+                $scope.luckYears = report.luckYears;
             });
     })
     .directive('linearChart', function ($window) {
-        return {
-            restrict: 'EA',
-            template: '<svg width="600" class="col-md-12 col-sm-12 col-xs-12" height="200"></svg>',
-            link: function (scope, elem, attrs) {
-                var dataToPlot = scope[attrs.chartData];
-                var padding = 20;
-                var pathClass = 'path';
-                var xScale, yScale, xAxisGen, yAxisGen, lineFun;
-
-                var d3 = $window.d3;
-                var rawSvg = elem.find('svg')[0];
-                var svg = d3.select(rawSvg);
-                console.log(svg[0]);
-                var width = svg.attr('width'),
-                    height = svg.attr('height');
-
-                function setChartParameters() {
-                    xScale = d3.scale.linear()
-                        .domain([dataToPlot[0].position, dataToPlot[dataToPlot.length - 1].position])
-                        .range([padding + 5, width - padding]);
-
-                    yScale = d3.scale.linear()
-                        .domain([0, d3.max(dataToPlot,
-                            function (d) {
-                                return d.value;
-                            }) + 1
-                        ])
-                        .range([height - padding, 0]);
-
-                    xAxisGen = d3.svg.axis()
-                        .scale(xScale)
-                        .orient('bottom')
-                        .ticks(dataToPlot.length - 1);
-
-                    yAxisGen = d3.svg.axis()
-                        .scale(yScale)
-                        .orient('left')
-                        .ticks(d3.max(dataToPlot,
-                                function (d) {
-                                    return d.value;
-                                }
-                            ) + 1);
-
-                    lineFun = d3.svg.line()
-                        .x(function (d) {
-                            return xScale(d.position);
-                        })
-                        .y(function (d) {
-                            return yScale(d.value);
-                        })
-                        .interpolate('linear');
-                }
-
-                function drawLineChart() {
-
-                    setChartParameters();
-
-                    svg.append('svg:g')
-                        .attr('class', 'x axis')
-                        .attr('transform', 'translate(0,180)')
-                        .call(xAxisGen);
-
-                    svg.append('svg:g')
-                        .attr('class', 'y axis')
-                        .attr('transform', 'translate(20,0)')
-                        .call(yAxisGen);
-
-                    svg.append('svg:path')
-                        .attr({
-                            d: lineFun(dataToPlot),
-                            'stroke': 'blue',
-                            'stroke-width': 2,
-                            'fill': 'none',
-                            'class': pathClass
-                        });
-                }
-
-                drawLineChart();
-            }
-        };
-    })
-    .directive('datesTable', function ($window) {
         return {
             restrict: 'EA',
             template: '<svg width="600" class="col-md-12 col-sm-12 col-xs-12" height="200"></svg>',
